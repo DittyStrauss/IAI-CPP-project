53e0533e0b72129ef55e11b60662802f20ff6802
DittyStrauss <ditty6592@gmail.com>
2022-07-28 14:15:18 +0300

    commit

---
+++ Common/Modem.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Component.h"
+class Modem:public Component
+{
+
+public:
+	Modem(typeDevice device) :Component(device,MODEM) {}
+};
+
---
+++ MessageGenerator/MessageGenerator/MessageGenerator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "MsgGenerator.h"
+
+int main()
+{
+	int rumTime;
+	std::cout << "Enter program running time!\n";
+	std::cin >>rumTime;
+	MsgGenerator msgGenerator(rumTime);
+	while (1) {
+		continue;
+	}
+}
+
+
